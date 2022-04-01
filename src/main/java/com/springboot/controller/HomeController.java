254e04f9e288fb1e9549af95f8cf2a1ee9772f14
repoforcode <sujeet.sharma49@gmail.com>
2022-04-01 13:15:40 +0530

    resolved runtime errors

--- src/main/java/com/springboot/controller/HomeController.java
+++ src/main/java/com/springboot/controller/HomeController.java
@@ -6,11 +6,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -30,17 +27,17 @@ public class HomeController {
 		mav.addObject("product", list);
 		return mav;
 	}
-	
+
 	@RequestMapping(value = "/getForm")
 	public ModelAndView getForm(Model model) {
-		Product product=new Product();
+		Product product = new Product();
 		model.addAttribute("product", product);
 		ModelAndView mav = new ModelAndView("createProductForm");
 		return mav;
-		
+
 	}
-	
-	@RequestMapping(value = "/save", method= RequestMethod.POST)
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public ModelAndView createProduct(@ModelAttribute("product") Product product) {
 		repo.save(product);
 		ModelAndView mav = new ModelAndView("Success");
